d26bf2a1478abfc5c14cb450b400d30f3adfffa6
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-09-03 23:29:53 +0900

    bug fix

--- webapp/mysql/db/3_Index.sql
+++ webapp/mysql/db/3_Index.sql
@@ -1,2 +1,2 @@
-CREATE INDEX chair_index ON chair (stock, id, price);
-CREATE INDEX estate_index ON estate (door_height, door_width, popularity, id);
+CREATE INDEX chair_index ON isuumo.chair (stock, id, price);
+CREATE INDEX estate_index ON isuumo.estate (door_height, door_width, popularity, id);

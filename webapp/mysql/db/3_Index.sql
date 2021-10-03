e8e9268a06958a465bde6270759997d9c7e9793b
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-10-04 00:06:42 +0900

    index貼り方修正

--- webapp/mysql/db/3_Index.sql
+++ webapp/mysql/db/3_Index.sql
@@ -1,2 +1,4 @@
-CREATE INDEX chair_index ON isuumo.chair (stock, id, price);
-CREATE INDEX estate_index ON isuumo.estate (door_height, door_width, popularity, id);
+CREATE INDEX chair_index_0 ON isuumo.chair (stock);
+CREATE INDEX chair_index_1 ON isuumo.chair (id, price);
+CREATE INDEX estate_index_0 ON isuumo.estate (door_height, door_width);
+CREATE INDEX estate_index_1 ON isuumo.estate (popularity, id);
